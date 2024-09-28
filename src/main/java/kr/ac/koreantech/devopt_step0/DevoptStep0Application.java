605f97a9290104e1ea98086e2a60d47f63b2e5d2
“indopop@naver” <indopop@naver.com>
2024-09-28 15:19:33 +0900

    first commit

---
+++ src/main/java/kr/ac/koreantech/devopt_step0/DevoptStep0Application.java
@@ -0,0 +1,13 @@
+package kr.ac.koreantech.devopt_step0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoptStep0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoptStep0Application.class, args);
+	}
+
+}
